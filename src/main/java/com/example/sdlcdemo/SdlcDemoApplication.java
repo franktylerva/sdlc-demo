88b8a74a0e69eed15ce0f7236eec5f83ce23b5c7
ftyler <ftyler.ctr@kr.af.mil>
2022-03-10 07:59:27 -0500

    Initial commit

---
+++ src/main/java/com/example/sdlcdemo/SdlcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdlcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdlcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdlcDemoApplication.class, args);
+	}
+
+}
