5c224909d2340ce6cb92c4b03c41e9af4a700a40
ftyler <ftyler.ctr@kr.af.mil>
2022-03-23 14:14:50 -0400

    Add index controller method.

--- src/main/java/com/example/sdlcdemo/SdlcDemoApplication.java
+++ src/main/java/com/example/sdlcdemo/SdlcDemoApplication.java
@@ -2,12 +2,19 @@ package com.example.sdlcdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class SdlcDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SdlcDemoApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String index() {
+		return "Hello!";
+	}
 }
