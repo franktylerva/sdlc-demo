7b577a3cae12e8739576b4db0e18599dfe06f9b3
ftyler <ftyler.ctr@kr.af.mil>
2022-06-16 07:32:55 -0400

    Another update.

--- src/main/java/com/example/sdlcdemo/SdlcDemoApplication.java
+++ src/main/java/com/example/sdlcdemo/SdlcDemoApplication.java
@@ -15,6 +15,6 @@ public class SdlcDemoApplication {
 
 	@GetMapping("/")
 	public String index() {
-		return "Hello World!  Testing another update.";
+		return "Hello World! Another update.";
 	}
 }
